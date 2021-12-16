2b7fde811fc4906e8cc3caab5325228554cfd6f5
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2021-12-16 15:12:57 +0200

    initializing

--- src/main/java/rw/ac/rca/termOneExam/service/CityService.java
+++ src/main/java/rw/ac/rca/termOneExam/service/CityService.java
@@ -17,23 +17,33 @@ public class CityService {
 	private ICityRepository cityRepository;
 	
 	public Optional<City> getById(long id) {
-		
-		return cityRepository.findById(id);
+		  Optional<City> city = cityRepository.findById(id);
+		      city.ifPresent(city1 -> city1.setFahrenheit(this.calculateFahrenheit(city1.getWeather())));
+			  return  city;
 	}
 
 	public List<City> getAll() {
-		
-		return cityRepository.findAll();
+		 List<City> allCity = cityRepository.findAll();
+		   allCity.forEach(city -> {
+			   city.setFahrenheit(this.calculateFahrenheit(city.getWeather()));
+		   });
+		return allCity;
 	}
 
 	public boolean existsByName(String name) {
-		
+
 		return cityRepository.existsByName(name);
 	}
 
 	public City save(CreateCityDTO dto) {
 		City city =  new City(dto.getName(), dto.getWeather());
-		return cityRepository.save(city);
+		City city1 = cityRepository.save(city);
+		 city1.setFahrenheit(this.calculateFahrenheit(city.getWeather()));
+		return city1 ;
+	}
+
+	private double calculateFahrenheit(double weather){
+		return (9/5 * weather) + 32;
 	}
 	
 
--- src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
+++ src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
@@ -1,5 +1,58 @@
 package rw.ac.rca.termOneExam.controller;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.utils.APICustomResponse;
+
+
 public class CityControllerIntegrationTest {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+		
+	@Test
+	public void getAll_success() throws JSONException {
+		String response = this.restTemplate.getForObject("/api/cities/all", String.class);
+		
+		//JSONAssert.assertEquals("[]", response, false);
+		JSONAssert.assertEquals("[{id:101},{id:102},{id:103},{id:104}]", response, false);
+	}
+	
+	@Test
+	public void getById_successObject() {
+		City city = this.restTemplate.getForObject("/api/cities/id/101", City.class);
+		
+		assertEquals("Item1", city.getName());
+		assertEquals(1000, city.getWeather());
+	}
+	@Test
+	public void getById_success() {
+		ResponseEntity<City> city = this.restTemplate.getForEntity("/api/cities/id/101", City.class);
+		
+		assertTrue(city.getStatusCode().is2xxSuccessful());
+		assertEquals("Item1", city.getBody().getName());
+		assertEquals(1000, city.getBody().getWeather());
+	}
+	
+	@Test
+	public void getById_404() {
+		ResponseEntity<APICustomResponse> item = 
+				this.restTemplate.getForEntity("/api/cities/id/101", APICustomResponse.class);
+	
+		assertTrue(item.getStatusCodeValue()==404);
+		assertFalse(item.getBody().isStatus());
+		assertEquals("Item not found", item.getBody().getMessage());
+	}
+	
 
 }
