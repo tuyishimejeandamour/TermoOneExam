ea8141813bee325945fa9a6abbc66329afa52049
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2021-12-16 17:15:39 +0200

    adding uti check functions

--- src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
+++ src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
@@ -1,9 +1,25 @@
 package rw.ac.rca.termOneExam.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CityServiceTest {
 
-@Mock
-	private CityRepository cityRepositoryMock;
+
+    @Mock
+	private ICityRepository cityRepositoryMock;
 	
 	@InjectMocks
 	private CityService cityService;
@@ -11,23 +27,22 @@ public class CityServiceTest {
     @Test
 		public void getAll() {
 
-		when(itemRepositoryMock.findAll()).thenReturn(Arrays.asList(new City(101,"musanze",15),
-				new City(2,"nyabihu",13)));
+		when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City("musanze",15), new City("nyabihu",13)));
 		assertEquals(15,cityService.getAll().get(0).getWeather());
 	}
     @Test
 	public void getById() {
 
-		when(itemRepositoryMock.getById(101)).thenReturn(new City(101,"musanze",15));
+		when(cityRepositoryMock.getById(101L)).thenReturn(new City("musanze",15));
 		assertEquals("musanze",cityService.getAll().get(0).getName());
 	}
 	
 	@Test
 	public void save() {
-		CreateCityDTO dto = new CreateCityDTO(101,"kirehe", 27);
-		City city = new City(101,"kirehe",27,);
-		when(itemRepositoryMock.existsByName(dto.getName())).thenReturn(true);
-		when(itemRepositoryMock.save(item)).thenReturn(city);
+		CreateCityDTO dto = new CreateCityDTO("kirehe", 27);
+		City city = new City("kirehe",27);
+		when(cityRepositoryMock.existsByName(dto.getName())).thenReturn(true);
+		when(cityRepositoryMock.save(city)).thenReturn(city);
 		
 		City insertdata = cityService.save(dto);
 		assertEquals("kirehe",insertdata.getName());
